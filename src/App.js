febd8c3bb539d0115ba83fd6071247c32c250e82
uzair <uzairshaikh5674@gmail.com>
2022-11-27 16:36:14 +0530

    i done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState } from 'react'
+import "./App.css";
 
 function App() {
-  return (
+  const [data, setdata] = useState('');
+  useEffect(() =>{
+    fetch('https://jsonplaceholder.typicode.com/users/1').then((response) => 
+    response.json()).then(setdata);
+    
+  },[setdata]);
+  console.log(data);
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text">Displaying Data in json Format</h1>
+
+   <pre className="text">{JSON.stringify(data, null, 2)}</pre>
+ 
     </div>
-  );
+  )
 }
 
 export default App;
